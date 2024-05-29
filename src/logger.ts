e450887fe209de8b820f6c0eb321e4542e831fa8
Themuuln <zerone.offical@gmail.com>
2024-05-29 17:00:50 +0800

    path update

---
+++ src/logger.ts
@@ -0,0 +1,22 @@
+export interface LoggerOptions {
+  prefix?: string;
+  isDev?: boolean;
+}
+
+const defaultOptions: LoggerOptions = {
+  prefix: '',
+  isDev: process.env.NODE_ENV === 'development',
+};
+
+export const createLogger = (options: LoggerOptions = {}) => {
+  const { prefix, isDev } = { ...defaultOptions, ...options };
+
+  return (...args: any[]) => {
+    if (isDev) {
+      console.log(prefix, ...args);
+    }
+  };
+};
+
+// Default logger
+export const clg = createLogger();
