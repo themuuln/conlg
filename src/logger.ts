3b9963624f5e2c36b822a8e53751646ba8d85e81
Themuuln <zerone.offical@gmail.com>
2024-06-18 14:04:26 +0800

    fixed

--- src/logger.ts
+++ src/logger.ts
@@ -1,25 +1,11 @@
-import * as path from 'path';
-import * as fs from 'fs';
-
 export interface LoggerOptions {
   prefix?: string;
   isDev?: boolean;
 }
 
-const loadConfig = (): LoggerOptions => {
-  const configPath = path.resolve(process.cwd(), 'conlg.config.js');
-  if (fs.existsSync(configPath)) {
-    return require(configPath);
-  }
-  return {};
-};
-
-const configOptions = loadConfig();
-
 const defaultOptions: LoggerOptions = {
   prefix: '',
   isDev: process.env.NODE_ENV === 'development',
-  ...configOptions,
 };
 
 export const createLogger = (options: LoggerOptions = {}) => {
