b7654fa50ff2b2fae816415f9ce847574a116d1f
Themuuln <zerone.offical@gmail.com>
2024-06-18 14:59:47 +0800

    minor changes

--- src/logger.ts
+++ src/logger.ts
@@ -1,20 +1,23 @@
 export interface LoggerOptions {
-  prefix?: string;
   isDev?: boolean;
 }
 
 const defaultOptions: LoggerOptions = {
-  prefix: '',
   isDev: process.env.NODE_ENV === 'development',
 };
 
 export const createLogger = (options: LoggerOptions = {}) => {
-  const { prefix, isDev } = { ...defaultOptions, ...options };
+  const { isDev } = { ...defaultOptions, ...options };
 
   return (...args: any[]) => {
     if (isDev) {
-      const loggedArgs = args.length === 1 && typeof args[0] === 'string' ? args[0] : args;
-      console.log(prefix, ...loggedArgs);
+      const newArgs = args.map((arg) => {
+        if (typeof arg === 'object') {
+          return JSON.stringify(arg, null, 2);
+        }
+        return arg;
+      });
+      console.log(...newArgs);
     }
   };
 };
